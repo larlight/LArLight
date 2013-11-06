76c34a1e14a94da6a9f77345cd91e5bd75856ad8
drinkingkazu <kazuhiro@nevis.columbia.edu>
2013-11-06 12:28:52 -0600

    Introducing the framework base for process/quality-control database interface --kazu

---
+++ AnaProcess/ProcessDB/ProcessDB-TypeDef.hh
@@ -0,0 +1,17 @@
+//
+// type-def file for ROOT
+//
+
+#ifndef PROCESSDB_TYPEDEF_HH
+#define PROCESSDB_TYPEDEF_HH
+
+// Header files included here
+#include "ProcessDBI.hh"
+#include "ProcessDBIManager.hh"
+#include "TableBase.hh"
+
+// Class forward declaration here
+class ProcessDBI;
+class ProcessDBIManager;
+class TableBase;
+#endif
