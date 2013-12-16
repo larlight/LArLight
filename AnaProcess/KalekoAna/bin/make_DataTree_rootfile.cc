282a6e6e15de77944b9167c9382ab17156821602
drinkingkazu <kazuhiro@nevis.columbia.edu>
2013-12-16 12:15:11 -0500

    Forgotten bin files --kazu

--- AnaProcess/KalekoAna/bin/make_DataTree_rootfile.cc
+++ AnaProcess/KalekoAna/bin/make_DataTree_rootfile.cc
@@ -3,7 +3,7 @@
 
 int main(){
 
-  KalekoAna k;
+  KalekoTrackAna k;
   std::string fname("../../../kaleko_input_files_LArLight/v3_ang0_dsout_reduced_Kalman3DSPS.root");
   int ang=0;
   int nevents=10;
