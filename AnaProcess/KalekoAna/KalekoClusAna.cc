a605783b4ebf76f753cad49709ac72372a1cfeab
David Kaleko <kaleko@nevis.columbia.edu>
2013-12-16 17:08:45 -0500

    Getting KalekoClusAna.cc working with ClusterPrepAna\!

--- AnaProcess/KalekoAna/KalekoClusAna.cc
+++ AnaProcess/KalekoAna/KalekoClusAna.cc
@@ -12,6 +12,8 @@ namespace kaleko {
     // Do all variable initialization you wish to do here.
     // If you need, you have an output root file pointer "_fout".
     larlight::ClusterAnaPrep::get()->initialize();
+    larlight::ClusterAnaPrep::get()->set_q_frac_cut(1.);
+    std::cout<<"Setting set_q_frac_cut to 1."<<std::endl;
     return true;
   }
   
@@ -23,9 +25,28 @@ namespace kaleko {
     // event-wise data. For a reference of pmt_wf_collection class instance, 
     // see the class index in the documentation.
 
+
     larlight::ClusterAnaPrep::get()->analyze(storage);
 
-  
+    const std::vector<larlight::cluster_ana_info> *u_clusters;
+    u_clusters = larlight::ClusterAnaPrep::get()->get_cluster_info(larlight::GEO::kU);
+    std::cout<<"The number of u_clusters is : "
+	     <<(int)u_clusters->size()
+	     <<std::endl;
+    std::cout<<"The start wire of the 0th cluster is : "
+	     <<u_clusters->at(0).start_wire
+	     <<" and the start time is : "
+	     <<u_clusters->at(0).start_time
+	     <<" while the min start time is : "
+	     <<u_clusters->at(0).start_time_min
+	     <<std::endl;
+    std::cout<<"The total sum charge in this clsuter is : "
+	     <<u_clusters->at(0).sum_charge
+	     <<std::endl;
+
+    std::cout<<"Other available variables in Analysis/ana_info_struct.hh"
+	     <<std::endl;
+
     return true;
   }
 
