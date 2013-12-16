537ed256755aad30ee9084d2df120012f60be33e
David Kaleko <kaleko@nevis.columbia.edu>
2013-12-16 13:00:24 -0500

    Removed KalekoTrackAna package, fixed kaleko_trackviewer.py to work with larlight and kaleko namespaces.

--- AnaProcess/KalekoAna/mac/kaleko_trackviewer.py
+++ AnaProcess/KalekoAna/mac/kaleko_trackviewer.py
@@ -18,10 +18,10 @@ if len(sys.argv) != 3:
     print
 
 # Create ana_processor instance
-my_proc=ana_processor()
+my_proc=larlight.ana_processor()
 
 # Specify IO mode
-my_proc.set_io_mode(storage_manager.READ)
+my_proc.set_io_mode(larlight.storage_manager.READ)
 #my_proc.set_io_mode(storage_manager.WRITE)
 #my_proc.set_io_mode(storage_manager.BOTH)
 
@@ -29,21 +29,21 @@ my_proc.set_io_mode(storage_manager.READ)
 # This time, we use a sample file prepared.
 my_proc.add_input_file(sys.argv[1])
 
-storage_manager.get().set_in_rootdir("scanner")
+larlight.storage_manager.get().set_in_rootdir("scanner")
 
 # Set output root file: this is a separate root file in which your
 # analysis module can store anything such as histograms, your own TTree, etc.
 my_proc.set_ana_output_file("my_empty_trackviewer_output.root")
 
 # Add analysis unit to process
-my_ana = TrackViewer()
+my_ana = larlight.TrackViewer()
 
 if sys.argv[2] == "Bezier":
-    my_ana.set_data_type(DATA.Bezier)
+    my_ana.set_data_type(larlight.DATA.Bezier)
 elif sys.argv[2] == "Kalman3DSPS":
-    my_ana.set_data_type(DATA.Kalman3DSPS)
+    my_ana.set_data_type(larlight.DATA.Kalman3DSPS)
 elif sys.argv[2] == "Kalman3DHit":
-    my_ana.set_data_type(DATA.Kalman3DHit)
+    my_ana.set_data_type(larlight.DATA.Kalman3DHit)
 else:
     print "Track reco algorithm must be Bezier, Kalman3DSPS, or Kalman3DHit. Try again."
 
@@ -89,7 +89,7 @@ while my_proc.process_event():
 
     for x in xrange(vData.size()):
         #if you want to look at angle b/t first two reco track points....
-        reco_theta = vData.at(x).direction_at(0).Theta() * UtilFunctions().DegreesPerRadian;
+        reco_theta = vData.at(x).direction_at(0).Theta() * kaleko.UtilFunctions().DegreesPerRadian;
         #if you want to look at angle b/t first and last reco track points...
         #reco_theta = (vData.at(x).direction_at(vData.at(x).n_point()-1) - vData.at(x).direction_at(0)).Theta()*UtilFunctions().DegreesPerRadian;
         #if you want to look at a weighted average angle (weighted by distance b/t points)...
