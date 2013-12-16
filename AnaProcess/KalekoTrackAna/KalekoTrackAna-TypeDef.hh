7eb482991ac16a80671bf0b0a2d20b6fc6fc059d
David Kaleko <kaleko@nevis.columbia.edu>
2013-12-16 11:56:28 -0500

    Trying to get KalekoTrackAna to work ... also edited AnaProcess/lib/anaproc_rootmap.sh

---
+++ AnaProcess/KalekoTrackAna/KalekoTrackAna-TypeDef.hh
@@ -0,0 +1,16 @@
+//
+// type-def file for ROOT
+//
+
+#ifndef KALEKOTRACKANA_TYPEDEF_HH
+#define KALEKOTRACKANA_TYPEDEF_HH
+
+// Header files included here
+#include "KalekoTrackAna.hh"
+#include "KalekoUtilFunc.hh"
+
+// Class forward declaration here
+class kaleko::KalekoTrackAna;
+class kaleko::UtilFunctions;
+
+#endif
