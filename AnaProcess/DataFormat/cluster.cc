98b70b637f5da0ef6af612b9e9ef5d887baf36cd
drinkingkazu <kazuhiro@nevis.columbia.edu>
2013-12-16 08:41:36 -0500

    UPDATE:
    	(*) Use wire number rather than channel number in ClusterViewer
    	(*) Include wire number in get_axis_range function
    
    	Code not yet tested because some other issue corrupted my root file...
    
    Kazu

--- AnaProcess/DataFormat/cluster.cc
+++ AnaProcess/DataFormat/cluster.cc
@@ -5,22 +5,39 @@
 
 namespace larlight{
   //############################################################################
-  void cluster::get_axis_range(std::vector<double> &wiremax, 
+  void cluster::get_axis_range(std::vector<double> &chmax,
+			       std::vector<double> &chmin,
+			       std::vector<double> &wiremax, 
 			       std::vector<double> &wiremin, 
 			       std::vector<double> &timemax, 
 			       std::vector<double> &timemin) const
   //############################################################################
   {
 
+    // Make sure input vector is of size wire plane with initial value -1 (if not yet set)
+    chmax.resize((GEO::kW+1),-1);
+    wiremax.resize((GEO::kW+1),-1);
+    timemax.resize((GEO::kW+1),-1);
+    chmin.resize((GEO::kW+1),-1);
+    wiremin.resize((GEO::kW+1),-1);
+    timemin.resize((GEO::kW+1),-1);
+
     //vtx is a vector of hits for one cluster?
     for(const auto &hit : fHitVector) {
 
-      int vw = hit.View();
-      
-      if(hit.Channel() > wiremax[vw]) wiremax[vw] = hit.Channel();
-      if(hit.Channel() < wiremin[vw]) wiremin[vw] = hit.Channel();
-      if(hit.EndTime() > timemax[vw]) timemax[vw] = hit.EndTime();
-      if(hit.StartTime() < timemin[vw]) timemin[vw] = hit.StartTime();
+      GEO::View_t  view    =  hit.View();
+      double       wire    =  (double)(hit.Wire());
+      double       ch      =  (double)(hit.Channel());
+      double       tstart  =  hit.StartTime();
+      double       tend    =  hit.EndTime();
+
+      if( wiremax[view] < 0 || wiremax[view] < wire )  wiremax[view] = wire;
+      if( chmax[view]   < 0 || chmax[view]   < ch   )  chmax[view]   = ch;
+      if( timemax[view] < 0 || timemax[view] < tend )  timemax[view] = tend;
+
+      if( wiremin[view] < 0 || wiremin[view] > wire   )  wiremin[view] = wire;
+      if( chmin[view]   < 0 || chmin[view]   > ch     )  chmin[view]   = ch;
+      if( timemin[view] < 0 || timemin[view] > tstart )  timemin[view] = tstart;
       
     }
 
@@ -29,17 +46,18 @@ namespace larlight{
 
 
   //############################################################################
-
-  void event_cluster::get_axis_range(std::vector<double> &wiremax, 
+  void event_cluster::get_axis_range(std::vector<double> &chmax,
+				     std::vector<double> &chmin,
+				     std::vector<double> &wiremax, 
 				     std::vector<double> &wiremin, 
 				     std::vector<double> &timemax, 
 				     std::vector<double> &timemin) const
   //############################################################################
   {
     //fCluster_v is a vector of all clusters in the event
-    for(auto clus : fCluster_v){
+    for(auto const &cluster : fCluster_v){
 
-	clus.get_axis_range(wiremax,wiremin,timemax,timemin);
+      cluster.get_axis_range(chmax,chmin,wiremax,wiremin,timemax,timemin);
 
     }
 
