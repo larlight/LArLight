c01b1b493ccec2fca1401e304e5a951381f90eda
David Caratelli <davidc1@uboonegpvm01.fnal.gov>
2014-02-05 21:40:17 -0600

    David C analysis skeleton

---
+++ AnaProcess/DavidAna/DavidAna-TypeDef.hh
@@ -0,0 +1,20 @@
+//
+// type-def file for ROOT
+//
+
+#ifndef DAVIDANA_TYPEDEF_HH
+#define DAVIDANA_TYPEDEF_HH
+
+// Header files included here
+#include "DavidAna.hh"
+//ADD_NEW_HEADER ... do not change this comment line
+
+// Class forward declaration here
+namespace larlight {
+
+  class DavidAna;
+
+//ADD_NEW_CLASS ... do not change this comment line
+
+}
+#endif
