bcaa1fd650b66c5b001ee863f42b262bb1752d87
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-03-10 21:39:49 -0500

    Removing SimpleTree fmwk ... now in a separate branch --kazu
    Former-commit-id: e1bb4fd8a3a46d58142746f4adb1f5422ccaf123

--- SimpleTree/Analysis/ana_base.cc
+++
@@ -1,6 +0,0 @@
-#ifndef ANA_BASE_CC
-#define ANA_BASE_CC
-
-#include "ana_base.hh"
-
-#endif
--- SimpleTree/DataFormat/mac/example_chain.py
+++
@@ -1,8 +0,0 @@
-from ROOT import *
-gSystem.Load("libDataFormat")
-
-k=storage_manager()
-k.add_in_filename("out.root")
-ch=k.get_chain()
-#ch.Scan("bezier.entry:kalman3dsps.entry")
-ch.MakeClass("aho")
