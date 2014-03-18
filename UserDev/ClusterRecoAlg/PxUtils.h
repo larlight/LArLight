63ac1a7d85639ebd92b2eca712786063758b7d4c
Corey Adams <corey.adams@yale.edu>
2014-03-18 14:59:54 -0400

    Adding the new template for cluster params alg, mods of PxUtils

--- UserDev/ClusterRecoAlg/PxUtils.h
+++ UserDev/ClusterRecoAlg/PxUtils.h
@@ -1,16 +1,28 @@
-struct point {
+struct PxPoint {
   public:
     double w; // wire distance in cm
     double t; // time distance in cm (drift distance)
-    double charge;  // charge of the hit
     unsigned int plane; // plane 0, 1, 2
    
-    point(){
+    PxPoint(){
 	plane=0;
 	w=0;
 	t=0;
-        charge = 0;
 	std::cout<< "This is the default point ctor." << std::endl;
     }
-}
+};
+
+struct PxHit : PxPoint {
+
+    double charge; //charge
+
+    PxHit(){
+        plane = 0;
+        w = 0;
+        t = 0;
+        charge = 0;
+        std::cout << "This is the $^_*&ing default ctor." << std::endl;
+    }
+
+};
 
