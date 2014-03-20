0cf1cbfeb0dcd6d049a2a1a4d9352ddd2f13b973
Jason St. John <jason.st.john@gmail.com>
2014-03-20 11:02:39 -0400

    Minor changes

--- UserDev/ClusterRecoUtil/ClusterParamsAlgNew.hh
+++ UserDev/ClusterRecoUtil/ClusterParamsAlgNew.hh
@@ -73,9 +73,6 @@ namespace cluster {
      * verticalness
      * rough_2d_slope
      * rough_2d_intercept
-     //would be good to have also the rough 2d for the high charge
-     //> rough_hc_2d_slope
-     //> rough_hc_2d_intercept
      * @param override [description]
      */
     //void GetRoughAxis(bool override=false);
