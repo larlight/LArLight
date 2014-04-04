d022fe6c4539556fabdf19504d8f599428279caa
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-04-04 17:59:39 -0500

    Updating TriggerSim with proposed TimeService and ElecClock which replaced SimpleClock --kazu

---
+++ UserDev/TriggerSim/TimeServiceConstants.h
@@ -0,0 +1,15 @@
+
+#ifndef ELECTIMECONSTANTS_H
+#define ELECTIMECONSTANTS_H
+
+#include <limits>
+#include <climits>
+
+namespace util {
+
+  // Invalid frame and sample
+  const unsigned int kMAX_UINT = std::numeric_limits<unsigned int>::max();
+
+}
+
+#endif
