b6c2d0ef97e5690994ae657f6d3f6edc6245f9d9
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-04-16 18:56:33 -0500

    Removing TriggerSim and ProcessDB: all LArSoft related development now goes to LArSoft branch --kazu

--- UserDev/TriggerSim/TimeService.cc
+++
@@ -1,9 +0,0 @@
-#ifndef TIMESERVICE_CC
-#define TIMESERVICE_CC
-
-#include "TimeService.h"
-
-namespace util {
-  TimeService* TimeService::_me = 0;
-}
-#endif
--- UserDev/TriggerSim/bin/tmp/empty.cc
+++
@@ -1,6 +0,0 @@
-#ifndef EMPTY_CLASS_NAME_HH
-#define EMPTY_CLASS_NAME_HH
-
-#include "Empty_Class_Name.hh"
-
-#endif
