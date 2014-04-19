671e24afab84c06a2e83d717557335644d03749d
Andrzej <andrzej.szelc@yale.edu>
2014-04-18 22:18:36 -0500

    add parameters to ClusterParams.hh

--- UserDev/ClusterRecoUtil/ClusterParams.hh
+++ UserDev/ClusterRecoUtil/ClusterParams.hh
@@ -52,6 +52,9 @@ namespace cluster{
     double N_Hits;
     double N_Hits_HC;
 
+    double modmeancharge;
+    double RMS_charge;
+    
     /**
        +1 means shower is "forward - start wire < end wire"
        -1 is backwards, 0 is undecided
@@ -86,6 +89,8 @@ namespace cluster{
       closing_angle                     = -999.999 ;
       closing_angle_charge_wgt          = -999.999 ;
       offaxis_hits                      = -999.999 ;
+      modmeancharge			 = -999.999 ;
+      RMS_charge 			 = -999.999 ;
       modified_hit_density              = -999.999 ;    
       N_Hits                            = -999.999 ;    
       N_Hits_HC                         = -999.999 ;    
@@ -104,35 +109,37 @@ namespace cluster{
 	       << "   start_point  ................ : (" << start_point.w <<","<<start_point.t<<")\n"
 	       << "   end_point  .................. : (" << end_point.w <<","<<end_point.t<<")\n"
 	       << "   sum_charge  ................. : "<< sum_charge << "\n"
-         << "   mean_charge   ............... : " << mean_charge << "\n"
-         << "   mean_x  ..................... : " << mean_x << "\n"
-         << "   mean_y  ..................... : " << mean_y << "\n"
-         << "   rms_x  ...................... : " << rms_x << "\n"
-         << "   rms_y  ...................... : " << rms_y << "\n"
-         << "   charge_wgt_x  ............... : " << charge_wgt_x << "\n"
-         << "   charge_wgt_y  ............... : " << charge_wgt_y << "\n"
-         << "   cluster_angle_2d  ........... : " << cluster_angle_2d << "\n"
-         << "   angle_2d  ................... : " << angle_2d         << "\n"
-         << "   eigenvalue_principal  ....... : " << eigenvalue_principal << "\n"
-         << "   eigenvalue_secondary  ....... : " << eigenvalue_secondary << "\n"
-         << "   multi_hit_wires  ............ : " << multi_hit_wires << "\n"
-         << "   N_Wires  .................... : " << N_Wires << "\n"
-         << "   N_Hits  ..................... : " << N_Hits << "\n"
-         << "   N_Hits_HC  .................. : " << N_Hits_HC << "\n"
-         << "   verticalness  ............... : " << verticalness << "\n"
-         << "   opening_angle  .............. : " << opening_angle << "\n"
-         << "   opening_angle_charge_wgt  ... : " << opening_angle_charge_wgt << "\n"
-         << "   closing_angle  .............. : " << closing_angle << "\n"
-         << "   closing_angle_charge_wgt  ... : " << closing_angle_charge_wgt << "\n"
-         << "   offaxis_hits  ............... : " << offaxis_hits << "\n"
-         << "   modified_hit_density  ....... : " << modified_hit_density << "\n"
-         << "   length  ..................... : " << length << "\n"
-         << "   width  ...................... : " << width << "\n"
-         << "   hit_density_1D  ............. : " << hit_density_1D << "\n"
-         << "   hit_density_2D  ............. : " << hit_density_2D << "\n"
-         << "   showerness  ................. : " << showerness << "\n"
-         << "   trackness  .................. : " << trackness << "\n"
-         << "   direction  .................. : " << direction << "\n";
+         << "   mean_charge   ............. : " << mean_charge << "\n"
+         << "   mean_x  ................... : " << mean_x << "\n"
+         << "   mean_y  ................... : " << mean_y << "\n"
+         << "   rms_x  .................... : " << rms_x << "\n"
+         << "   rms_y  .................... : " << rms_y << "\n"
+         << "   charge_wgt_x  ............. : " << charge_wgt_x << "\n"
+         << "   charge_wgt_y  ............. : " << charge_wgt_y << "\n"
+         << "   cluster_angle_2d  ......... : " << cluster_angle_2d << "\n"
+         << "   angle_2d  ................. : " << angle_2d         << "\n"
+         << "   eigenvalue_principal  ..... : " << eigenvalue_principal << "\n"
+         << "   eigenvalue_secondary  ..... : " << eigenvalue_secondary << "\n"
+         << "   multi_hit_wires  .......... : " << multi_hit_wires << "\n"
+         << "   N_Wires  .................. : " << N_Wires << "\n"
+         << "   N_Hits  ................... : " << N_Hits << "\n"
+         << "   N_Hits_HC  ................ : " << N_Hits_HC << "\n"
+         << "   verticalness  ............. : " << verticalness << "\n"
+         << "   opening_angle  ............ : " << opening_angle << "\n"
+         << "   opening_angle_charge_wgt  . : " << opening_angle_charge_wgt << "\n"
+         << "   closing_angle  ............ : " << closing_angle << "\n"
+         << "   closing_angle_charge_wgt  . : " << closing_angle_charge_wgt << "\n"
+         << "   offaxis_hits  ............. : " << offaxis_hits << "\n"
+         << "   modified_hit_density  ..... : " << modified_hit_density << "\n"
+ 	 << "   modified mean charge ...... : " << modmeancharge << "\n"
+ 	 << "   charge_RMS ................ : " << RMS_charge << "\n"
+         << "   length  ................... : " << length << "\n"
+         << "   width  .................... : " << width << "\n"
+         << "   hit_density_1D  ........... : " << hit_density_1D << "\n"
+         << "   hit_density_2D  ........... : " << hit_density_2D << "\n"
+         << "   showerness  ............... : " << showerness << "\n"
+         << "   trackness  ................ : " << trackness << "\n"
+         << "   direction  ................ : " << direction << "\n";
     }
 
   };
