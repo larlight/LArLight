8b2f7b813c0e4ca36b05da87317e126266644f63
David <david@david-mini.(none)>
2014-04-23 16:24:07 -0500

    more polygon merging algorithms --David Caratelli

---
+++ UserDev/ClusterRecoUtil/CMAlgoStartInPoly.cc
@@ -0,0 +1,64 @@
+#ifndef CMALGOSTARTINPOLY_CC
+#define CMALGOSTARTINPOLY_CC
+
+#include "CMAlgoStartInPoly.hh"
+
+namespace cluster {
+
+  CMAlgoStartInPoly::CMAlgoStartInPoly()
+  {
+    // Nothing to be done in the base class
+    this->reconfigure();
+
+    _wire_2_cm = larutil::GeometryUtilities::GetME()->WireToCm();
+    _time_2_cm = larutil::GeometryUtilities::GetME()->TimeToCm();
+
+    SetMinHitsCut(40);
+
+  }
+
+
+  void CMAlgoStartInPoly::reconfigure(){
+
+    //not sure what needs to be reset/reconfigured for this algo
+    
+  }//end reconfigure function
+
+  
+  bool CMAlgoStartInPoly::Merge(const cluster_params &cluster1,
+				     const cluster_params &cluster2)
+  {
+
+    //If start point of one cluster is inside the other's polygon
+    //boundary --> then merge!
+    
+    float w_start1 = cluster1.start_point.w * _wire_2_cm;
+    float t_start1 = cluster1.start_point.t * _time_2_cm;
+
+    float w_start2 = cluster2.start_point.w * _wire_2_cm;
+    float t_start2 = cluster2.start_point.t * _time_2_cm;
+
+    //make Point() for the two start points
+    std::pair<float,float> start1;
+    std::pair<float,float> start2;
+    start1 = std::make_pair( w_start1, t_start1);
+    start2 = std::make_pair( w_start2, t_start2);
+
+    //check if start point for cluster1 is in Polygon of cluster2
+    if ( ( cluster2.PolyObject.PointInside( start1) ) and (cluster1.N_Hits > _MinHits) ){
+      std::cout << "Start point of Cluster 1 in Polygon of Cluster 2!" << std::endl;
+      return true;
+    }
+    //check if start point for cluster1 is in Polygon of cluster2
+    if ( ( cluster1.PolyObject.PointInside( start2) ) and (cluster1.N_Hits > _MinHits) ){
+      std::cout << "Start point of Cluster 2 in Polygon of Cluster 1!" << std::endl;
+      return true;
+    }
+
+    return false;
+  }
+
+
+}
+
+#endif
