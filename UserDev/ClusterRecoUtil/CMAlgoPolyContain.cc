291f7f5aef3eb75ecc63c28666118af047a47d4d
David <david@david-mini.(none)>
2014-04-25 00:06:09 -0500

    fixing bug with CMAlgoPolyContain --David Caratelli

--- UserDev/ClusterRecoUtil/CMAlgoPolyContain.cc
+++ UserDev/ClusterRecoUtil/CMAlgoPolyContain.cc
@@ -22,15 +22,19 @@ namespace cluster {
   bool CMAlgoPolyContain::Merge(const cluster_params &cluster1,
 				     const cluster_params &cluster2)
   {
+
+    if ( (cluster1.PolyObject.Size() < 3) or (cluster2.PolyObject.Size() < 3) )
+      return false;
+
     //if either polygon is fully contained in other
     //then return true! --> MERGE!
-    if ( cluster1.PolyObject.Contained(cluster2.PolyObject) or
-	 cluster2.PolyObject.Contained(cluster1.PolyObject) )
+    if ( (cluster1.PolyObject.Contained(cluster2.PolyObject)) or
+	 (cluster2.PolyObject.Contained(cluster1.PolyObject)) )
       return true;
     else
       return false;
   }
-
+  
 
 }
 
