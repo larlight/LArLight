ae2647609d7403655b6d7efdb0332658390c532a
David Kaleko <kaleko@nevis.columbia.edu>
2014-04-30 17:16:36 -0400

    changes to mcshowerlookback module for comparing reco hits to mcshower objects through IDE mappings, etc. I will have to fix code depending on it (I think only BryceToy)

--- UserDev/McshowerLookback/McshowerLookback.cc
+++ UserDev/McshowerLookback/McshowerLookback.cc
@@ -3,79 +3,115 @@
 
 #include "McshowerLookback.hh"
 
-McshowerLookback::McshowerLookback(){
-  std::cout<<"Hello world!"<<std::endl;
-}
 
-////////////////
-//Make map of trackIDs for each shower
-////////////////
-void McshowerLookback::FillShowerMap(larlight::event_mcshower* my_mcshow, std::map<unsigned long, unsigned short> &shower_idmap){
-  int num_mcshow=0;
+///////////////////////////////////////////////////
+//Make map of trackIDs for each shower           //
+///////////////////////////////////////////////////
+void McshowerLookback::FillShowerMap(larlight::event_mcshower* my_mcshow, 
+				     std::map<UInt_t,UInt_t> &shower_idmap)
+{
+  UInt_t mcshower_index = 0;
+  
   for(auto this_mcshow : *my_mcshow){
-    num_mcshow++;
-    auto this_mom = (std::vector<Double_t>)this_mcshow.MotherMomentum();
-    for(auto this_trackid : (std::vector<UInt_t>)this_mcshow.DaughterTrackID()){
-      //std::cout<<this_trackid<<std::endl;
-      //shower_idmap.insert(this_trackid,num_mcshow);
-      shower_idmap.insert(std::pair<unsigned long,unsigned short>(this_trackid,num_mcshow));
-    }
+
+    for(auto this_trackid : (std::vector<UInt_t>)this_mcshow.DaughterTrackID())
+      shower_idmap.insert(std::pair<UInt_t,UInt_t>(this_trackid,mcshower_index));
+    
+    mcshower_index++;
   }
+
 }
 
-////////////////
-//Make map of simchannel objects for each channel
-////////////////
-void McshowerLookback::FillSimchMap(larlight::event_simch* my_simch, std::map<unsigned short, larlight::simch> &simch_map){
-  for(auto this_simch : *my_simch){
-    //simch_map.insert();
-    simch_map.insert(std::pair<unsigned short,larlight::simch>(this_simch.Channel(),this_simch));
-  }
+
+///////////////////////////////////////////////////
+//Make map of simchannel objects for each channel//
+///////////////////////////////////////////////////
+void McshowerLookback::FillSimchMap(larlight::event_simch* my_simch, 
+				    std::map<UShort_t, larlight::simch> &simch_map){
+
+  for(auto this_simch : *my_simch)
+    simch_map.insert(std::pair<UShort_t,larlight::simch>(this_simch.Channel(),this_simch));
+  
 }
 
-////////////////
-//Find % of each shower in a single hit
-////////////////
-void McshowerLookback::MatchHitsAll(larlight::hit this_hit, std::map<unsigned short, larlight::simch> simch_map, std::map<unsigned long, unsigned short> shower_idmap, unsigned int num_mcshow){
+///////////////////////////////////////////////////
+//Find % of each shower in a single hit          //
+///////////////////////////////////////////////////
+std::vector<float> McshowerLookback::MatchHitsAll(const larlight::hit &this_hit, 
+						  std::map<UShort_t, larlight::simch> &simch_map, 
+						  std::map<UInt_t,UInt_t> &shower_idmap, 
+						  std::vector<UInt_t> MCShower_indices)
+{
   //std::cout<<"Going through Here?"<<std::endl;
+
   //setup variables for holding charge info from various mcshowers
+
+  //total MC charge deposited on that hit's wire during that hit's start->end time
   float tot_ides_charge = 0;
-  //float part_ides_charge[(const int)(num_mcshow+1)];
-  //for(int i=0;i<num_mcshow+1;i++)
-  //  part_ides_charge[i]=0;
+  //amount of MC charge deposited "" "" belonging to a given MCShower
+  std::vector<float> part_ides_charge((size_t)MCShower_indices.size(),0.);
+  //amount of MC charge deposited "" "" belonging to MCShower not in the map
+  float part_ides_charge_unknown_MCShower = 0;
+
   
   //Search for simchannels on hit's channel number
-  //auto simch_iter = simch_map.find(this_hit.Channel());
-  //if(simch_iter==simch_map.end()) std::cerr<<"Hit has no matched simchannel!!"<<std::endl;
-  
-  //else{
-  //  //Matched, so try to find ides
-  //  auto matchedsimch = (*simch_iter).second;
-  //  auto matchedide = (std::vector<larlight::ide>)matchedsimch.TrackIDsAndEnergies(this_hit.StartTime(),this_hit.EndTime());
-  //  //Assign ide charge to a particular MCShower using channel:mcshower mapping                                                                              
-  //  for(auto this_ide : matchedide){
-  //    unsigned int this_trackID = abs(this_ide.trackID);
-  //    tot_ides_charge+=this_ide.numElectrons;
-  //    auto show_iter=shower_idmap.find(this_trackID);
-  //    if(show_iter==shower_idmap.end()){
-  //	part_ides_charge[0]+=this_ide.numElectrons;
-  //    }
-  //    else{
-  //	part_ides_charge[(*show_iter).second]+=this_ide.numElectrons;
-  //    }
-  //  }
-  //}
+  auto simch_iter = simch_map.find(this_hit.Channel());
+  if(simch_iter==simch_map.end()) 
+    std::cerr<<"Hit has no matched simchannel!!"<<std::endl;
 
-  //std::vector<float> fractions;  
-  //for(int i=0;i<num_mcshow+1;i++){
-  //  if(tot_ides_charge>0) std::cout<<part_ides_charge[i]<<std::endl;
-  //  //fractions.push_back(part_ides_charge[i]/tot_ides_charge);
-  //  //else std::cerr<<"total ide charge less than 1?!"<<std::endl;
-  //}    
-  //std::cout<<"------------"<<std::endl;
-  ////fractions.push_back(tot_ides_charge);
-  
-  ////return fractions;
-}
+  else{
+
+    //Matched, so try to find ides
+    auto matchedsimch = (*simch_iter).second;
+    auto matchedides = (std::vector<larlight::ide>)matchedsimch.TrackIDsAndEnergies(this_hit.StartTime(),this_hit.EndTime());
+
+    //Assign ide charge to a particular MCShower using channel:mcshower mapping
+    //loop over the (MC) IDEs (particles) that correspond to this reconstructed hit
+    for(auto this_ide : matchedides){
+      unsigned int this_trackID = abs(this_ide.trackID);
+      tot_ides_charge+=this_ide.numElectrons;
+
+      //find this IDE's trackID in the shower mapping to get its MCShower index
+      auto show_iter=shower_idmap.find(this_trackID);
+      //if you can't find the IDE's trackID in the MCShower mapping
+      //(it was a particle not belonging to a shower, IE excited argon decay photon)
+      if(show_iter==shower_idmap.end()){
+	part_ides_charge_unknown_MCShower+=this_ide.numElectrons;
+      }
+      else{
+	//note: if the code is crashing, it's likely here
+	part_ides_charge.at((*show_iter).second)+=this_ide.numElectrons;
+      }
+
+    }//end loop over matchedides
+
+  }//end finding simch correspond to reco hit's cahnnel
+
+  if(tot_ides_charge<=0) {
+    std::cerr<<"something is bad. tot_ides_charge is "<<tot_ides_charge<<"... less than one?!"<<std::endl;
+    std::vector<float> bad(1,-1);
+    return bad;
+  }
+
+
+  //debug
+  //  std::cout<<"debug: finished and filled part_ides_charge is (";
+  //  for(int j = 0; j < part_ides_charge.size(); ++j)
+  //    std::cout<<part_ides_charge.at(j)<<", ";
+  //  std::cout<<"), with size "<<part_ides_charge.size()<<std::endl;
+
+
+
+  //compute the actual fractions function returns
+  std::vector<float> fractions;  
+  for(int i=0;i<(const int)MCShower_indices.size();i++){
+    fractions.push_back(part_ides_charge.at(i)/tot_ides_charge);
+  }
+  fractions.push_back(part_ides_charge_unknown_MCShower/tot_ides_charge);
+
+  return fractions;
+
+}//end MatchHitsAll()
+
 
 #endif
--- UserDev/McshowerLookback/McshowerLookback.hh
+++ UserDev/McshowerLookback/McshowerLookback.hh
@@ -29,11 +29,31 @@ class McshowerLookback{
 public:
 
   /// Default constructor
-  McshowerLookback();
+  McshowerLookback(){};
 
-  void FillShowerMap(larlight::event_mcshower* my_mcshow, std::map<unsigned long, unsigned short> &shower_idmap);
-  void FillSimchMap(larlight::event_simch* my_simch, std::map<unsigned short, larlight::simch> &simch_map);
-  void MatchHitsAll(larlight::hit this_hit, std::map<unsigned short, larlight::simch> simch_map, std::map<unsigned long, unsigned short> shower_idmap, unsigned int num_mcshow);
+  /// Takes in event_mcshowers and fills a map of 
+  /// (G4trackid of daughter particle => MCShower index in ev_mcshower)
+  void FillShowerMap(larlight::event_mcshower* my_mcshow, 
+		     std::map<UInt_t,UInt_t> &shower_idmap);
+
+  /// Takes in event_simchannels and fills a map of
+  /// (channel => larlight::simch)
+  void FillSimchMap(larlight::event_simch* my_simch, 
+		    std::map<UShort_t, larlight::simch> &simch_map);
+
+  /// Takes in a single hit, two maps, and a list of MCShower indices and returns 
+  /// a vector of charge fractions from the different MCShowers the hit belongs to.
+  /// For example, if the list of MCShowers indicies was (0, 1, 3)
+  /// and the hit's charge is 20% in MCShower 0, 70% in MCShower1, 
+  /// 0% in MCShower3, and 10% in an unknown MCShower, 
+  /// the returned vector is (0.2, 0.8, 0.0, 0.10)
+  /// (the unknown MCShower is always the last entry in the return vector)
+  std::vector<float> MatchHitsAll(const larlight::hit &this_hit, 
+				  std::map<UShort_t, larlight::simch> &simch_map, 
+				  std::map<UInt_t,UInt_t> &shower_idmap, 
+				  std::vector<UInt_t> MCShower_indices);
+
+
   //std::vector<double> MatchHitsAll;
   //double MatchClusOne;
   //std::vector<double> MatchClusAll;
