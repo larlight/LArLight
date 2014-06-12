a4c43c7cdf12aff8a04163644e79ae957abae529
David Kaleko <kaleko@nevis.columbia.edu>
2014-06-12 12:33:21 -0400

    finalized preliminary merging algos.

--- UserDev/ClusterStudy/mac/view_prelim_merger.py
+++ UserDev/ClusterStudy/mac/view_prelim_merger.py
@@ -31,65 +31,56 @@ my_proc.set_ana_output_file("")
 raw_viewer   = larlight.ClusterViewer()
 merge_viewer = larlight.MergeViewer()
 
-
+########################################
+# merge viewer parameters
+########################################
 merge_viewer.SetPrintClusterInfo(True)
+merge_viewer.SetMinHitsToDraw(0)
+merge_viewer.SetDrawPolygon(True)
+merge_viewer.SetDrawOnlyTracks(False)
 
 ########################################
 # attach merge algos here
 ########################################
+
+## PROHIBIT ALGOS ##
 prohib_array = cluster.CMAlgoArray()
 
 tracksep_prohibit = cluster.CMAlgoTrackSeparate()
-tracksep_prohibit.SetMinNumHits(30)
-tracksep_prohibit.SetMinAngleDiff(0.1)
-tracksep_prohibit.SetMaxOpeningAngle(30)
-tracksep_prohibit.SetMinLength(10.)
-tracksep_prohibit.SetMaxWidth(25.)
-tracksep_prohibit.SetDebug(False)
-tracksep_prohibit.SetVerbose(False)
+tracksep_prohibit.SetUseEP(True)
 prohib_array.AddAlgo(tracksep_prohibit,False)
 
 outofcone_prohibit = cluster.CMAlgoOutOfConeSeparate()
-outofcone_prohibit.SetDebug(False)
-outofcone_prohibit.SetVerbose(False)
+outofcone_prohibit.SetMaxAngleSep(20.)
 prohib_array.AddAlgo(outofcone_prohibit,False)
 
 merge_viewer.GetManager().AddSeparateAlgo(prohib_array)
 
-#algos that require AND condition have to be added in array
-#----------------------------------------------------------
-algo_array = cluster.CMAlgoArray()
+## MERGE ALGOS ##
+merge_array = cluster.CMAlgoArray()
 
-angalg = cluster.CMAlgoAngleCompat()
-angalg.SetVerbose(False)
-angalg.SetDebug(False)
-angalg.SetAllow180Ambig(False)
-angalg.SetUseOpeningAngle(False)
-angalg.SetAngleCut(3.)
-
-#False here means use "OR" condition
-algo_array.AddAlgo(angalg,False)
-
-algo = cluster.CMAlgoShortestDist()
-algo.SetVerbose(False)
-algo.SetDebug(False)
-algo.SetMinHits(10)
-algo.SetSquaredDistanceCut(5.)
-
-algo_array.AddAlgo(algo,False)
+shortdist_alg = cluster.CMAlgoShortestDist()
+shortdist_alg.SetMinHits(10)
+shortdist_alg.SetSquaredDistanceCut(5.)
+merge_array.AddAlgo(shortdist_alg,False)
 
 trackblob = cluster.CMAlgoStartTrack()
+merge_array.AddAlgo(trackblob,False)
 
-algo_array.AddAlgo(trackblob,False)
+pcontain = cluster.CMAlgoPolyContain()
+merge_array.AddAlgo(pcontain,False)
 
-merge_viewer.GetManager().AddMergeAlgo(algo_array)
-#----------------------------------------------------------
-
-merge_viewer.GetManager().MergeTillConverge(False)
+merge_viewer.GetManager().AddMergeAlgo(merge_array)
 
 ########################################
 # done attaching merge algos
 ########################################
+
+########################################
+# cmerge manager parameters
+########################################
+merge_viewer.GetManager().MergeTillConverge(True)
+
 
 
 my_proc.add_process(raw_viewer)
@@ -116,18 +107,6 @@ while true:
     raw_viewer.DrawAllClusters();
 
     merge_viewer.DrawAllClusters();
-
-
-#    for plane in xrange(larutil.Geometry.GetME().Nplanes()):
-#
-#        print "    Plane:", plane
-#        
-#        for cindex in xrange(merge_viewer.ClusterCount(plane)):
-#
-#            print "        Cluster:",cindex
-#            merge_viewer.DrawOneCluster(plane,cindex)
-#            sys.stdin.readline()
-#    
 
     print "    Hit enter to go next event..."
     sys.stdin.readline()
