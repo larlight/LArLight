7e4d3842e6789d01ba3286c6c98ff30d7f2e990f
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-06-20 10:17:21 -0500

    Make it easier to read a bit --kazu

--- UserDev/CMTool/mac/view_matcher.py
+++ UserDev/CMTool/mac/view_matcher.py
@@ -21,10 +21,6 @@ my_proc.set_io_mode(larlight.storage_manager.READ)
 my_proc.add_input_file(filename)
 
 larlight.storage_manager.get().set_in_rootdir("scanner")
-larlight.storage_manager.get().set_data_to_read(larlight.DATA.MCTruth,False)
-larlight.storage_manager.get().set_data_to_read(larlight.DATA.Shower,False)
-larlight.storage_manager.get().set_data_to_read(larlight.DATA.Calorimetry,False)
-larlight.storage_manager.get().set_data_to_read(larlight.DATA.UserInfo,False)
 
 my_proc.set_ana_output_file("")
 
