9708f2a33be9504b20197413032aef3ed143f87c
Ariana Hackenburg <ariana.hackenburg@yale.edu>
2014-07-22 11:45:19 -0500

    Small improvement to TimeOverlap algo --ahack

--- UserDev/CMTool/CFAlgoTimeOverlap.cc
+++ UserDev/CMTool/CFAlgoTimeOverlap.cc
@@ -9,9 +9,10 @@ namespace cmtool {
   CFAlgoTimeOverlap::CFAlgoTimeOverlap() : CFloatAlgoBase()
   //-------------------------------------------------------
   {
-		_time_ratio_cut 		= 0.1 ;  //Cuts thus far unoptimized
-		_time_difference_cut	= 20 ; 
-		_start_time_cut 		= 20 ;
+		SetRatioCut(0.001) ; //(0.095) ;  
+		SetStartTimeCut(10) ;
+		SetDebug(true) ;
+		SetVerbose(false) ;
 
   }
 
@@ -30,30 +31,42 @@ namespace cmtool {
 	double ratio				 = 1;
 	double time_difference		 = 0;
 	double max_time_difference	 = 0;
-	
+	double max_charge			 = 0;
+	double charge_ratio 		 = 1;	
+	double adjusted_charge_ratio = 1;
+
     //Preserve location in time space. Cut clusters that have similar time differences,
 	// but hit wires at very different times. 
 	double start_t				 = 0;
 	double end_t				 = 0;
 	double prev_start_t 		 = 0;
-	_verbose 					 = false; 
+	double prev_end_t 		 = 0;
 
 	for(auto const& c : clusters){
 
-		//Calculate time ratio for events with > 20 hits
-			time_difference  = c->GetParams().start_point.t - c->GetParams().end_point.t ; 
+		auto charge = c->GetParams().sum_charge ;		
+
+		time_difference  = c->GetParams().start_point.t - c->GetParams().end_point.t ; 
 	
-			if(time_difference < 0) 
-				time_difference *= -1; 
+		if(time_difference < 0) 
+			time_difference *= -1; 
+		
+		if(max_time_difference < time_difference)
+			max_time_difference = time_difference ;
+
+		if(max_charge < charge)
+			max_charge = charge ;
 	
-			if(max_time_difference < time_difference)
-				max_time_difference = time_difference ;
 	}
 
-
 	 for(auto const& c: clusters){
-		if(c->Plane()==0)
-			ratio=1; 
+		if(c->Plane()==0){
+			ratio=1;
+			charge_ratio=1;
+		 }
+		
+		double length = c->GetParams().length ;
+		auto charge = c->GetParams().sum_charge ;		
 	
      		//Make start_t always smaller
 			if(c->GetParams().start_point.t > c->GetParams().end_point.t){
@@ -67,36 +80,46 @@ namespace cmtool {
 				
 			if(prev_start_t ==0)
 				prev_start_t = start_t ;
+			if(prev_end_t ==0)
+				prev_end_t = end_t ;
 
 			time_difference  = end_t - start_t ; 
-	//		if( max_time_difference <= _time_difference_cut )
-	//			ratio *= 1 ;
-	//		else
+
 			ratio *= time_difference/max_time_difference ;
+//			charge_ratio *= charge/max_charge ;
+//			adjusted_charge_ratio = charge_ratio * ratio ;
+	
+			ratio *= charge_ratio ;
 	
 			//If current cluster's start time is not within some range of the previous cluster's start time,
 			//modify ratio to disallow matching
-			if( start_t < (prev_start_t - _start_time_cut) || start_t > (prev_start_t + _start_time_cut))
-					ratio*= 0.5;
-
-
+			
+			if( (start_t > (prev_start_t - _start_time_cut) && start_t < (prev_start_t + _start_time_cut)) 
+			   || (end_t > (prev_end_t - _start_time_cut) && end_t < (prev_end_t + _start_time_cut) )
+			   || (length >25 && start_t >(prev_start_t - 2*_start_time_cut) && start_t < (prev_start_t + 2*_start_time_cut) ) )
+				ratio *= 1;
+			else	
+				ratio*= 0.001;
 
 			prev_start_t = start_t ;
-
-	   		
-			if( _verbose && ratio > _time_ratio_cut ){	
+			prev_end_t 	 = end_t ;	
+		
+			if( _verbose ){//&& ratio > _time_ratio_cut ){
 				std::cout<<"\nPLANE: "<<c->Plane() ;
 				std::cout<<"\nStart point: "<<start_t<<std::endl;
 				std::cout<<"End Point: "<<end_t  <<std::endl;
-				std::cout<<"Previous start time: "<<prev_start_t<<std::endl;
+			//	std::cout<<"Previous start time: "<<prev_start_t<<std::endl;
+				std::cout<<"Time diff: "<<time_difference<<std::endl;
 				std::cout<<"Max time diff: "<<max_time_difference<<std::endl;
-				std::cout<<"ratio for each cluster: "<<ratio<<std::endl;
-				
+				std::cout<<"Ratio for each cluster: "<<ratio<<std::endl;
+//				std::cout<<"Charge: "<<charge<<std::endl;
+//				std::cout<<"Charge Ratio: "<<charge_ratio<<std::endl;
+//				std::cout<<"Adjusted Charge Ratio: "<<adjusted_charge_ratio<<std::endl;
+				std::cout<<"Length and Width: "<<c->GetParams().length<<", "<<c->GetParams().width<<std::endl;
 				} 
 
 		}
 
-//	std::cout<<"*****************End of a cluster! "<<std::endl;
 	if(_verbose && ratio> _time_ratio_cut)
 		std::cout<<"**************************FOUND A MATCH . ratio is: "<<ratio<<"\n\n\n"<<std::endl;
  
