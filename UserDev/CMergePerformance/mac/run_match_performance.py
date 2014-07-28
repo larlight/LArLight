be973e8e1ead16eec0c312b67aabbad17cc01bfb
Ariana Hackenburg <ariana.hackenburg@yale.edu>
2014-07-28 16:13:57 -0500

    Some temporary changes to 3DAngle algo --ahack

--- UserDev/CMergePerformance/mac/run_match_performance.py
+++ UserDev/CMergePerformance/mac/run_match_performance.py
@@ -5,17 +5,17 @@ from ROOT import larlight as fmwk
 processor = fmwk.ana_processor()
 
 processor.set_io_mode(fmwk.storage_manager.READ)
-
+#Getting rid of segfaults likea boss
 ana_module = fmwk.CMatchPerformance()
 
 priority_algo = cmtool.CPAlgoNHits()
 priority_algo.SetMinHits(20)
 
 algo_array = cmtool.CFAlgoArray()
-algo_array.SetMode(cmtool.CFAlgoArray.kSimpleAddition)
-algo_array.AddAlgo(cmtool.CFAlgoTimeOverlap())
+#algo_array.SetMode(cmtool.CFAlgoArray.kPositiveAddition)
+#algo_array.AddAlgo(cmtool.CFAlgoTimeOverlap())
 #algo_array.AddAlgo(cmtool.CFAlgoStartPointMatch())
-#algo_array.AddAlgo(cmtool.CFAlgo3DAngle())
+algo_array.AddAlgo(cmtool.CFAlgo3DAngle())
 #algo_array.AddAlgo(cmtool.CFAlgoZOverlap())
 
 match_algo = cmtool.CFAlgoStartPointMatch()
