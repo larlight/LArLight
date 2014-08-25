ae48db051f508fbe193327734931108b820ca22f
grossora <grossora.neutrino@gmail.com>
2014-08-25 17:02:23 -0500

    Ryans time prof hh

---
+++ UserDev/CMTool/CFAlgoTimeProf.hh
@@ -0,0 +1,81 @@
+/**
+ * \file CFAlgoTimeProf.hh
+ *
+ * \ingroup CMTool
+ * 
+ * \brief Class def header for a class CFAlgoTimeProf
+ *
+ * @author ryan_NAME
+ */
+
+/** \addtogroup CMTool
+
+    @{*/
+#ifndef CFALGOTIMEPROF_HH
+#define CFALGOTIMEPROF_HH
+
+#include "CFloatAlgoBase.hh"
+
+namespace cmtool {
+  /**
+     \class CFAlgoTimeProf
+     User implementation for CFloatAlgoBase class
+     doxygen documentation!
+  */
+  class CFAlgoTimeProf : public CFloatAlgoBase {
+    
+  public:
+    
+    /// Default constructor
+    CFAlgoTimeProf();
+    
+    /// Default destructor
+    virtual ~CFAlgoTimeProf(){};
+
+    //
+    // Author should be aware of 3 functions at least: Float, Report, 
+    // and Reset. More possibly-useful functions can be found in the later 
+    // part but commented out. All of these functions are virtual and defined
+    // in the base class.
+
+    virtual float Float(const std::vector<const cluster::ClusterParamsAlgNew*> &clusters);
+
+    virtual void Report();
+    
+    /// Function to reset the algorithm instance, called together with manager's Reset()
+    virtual void Reset();
+
+    /**
+       Optional function: called at the beginning of 1st iteration. This is called per event.
+     */
+    //virtual void EventBegin(const std::vector<cluster::ClusterParamsAlgNew> &clusters);
+
+    /**
+       Optional function: called at the end of event ... after the last merging iteration is over.
+     */
+    //virtual void EventEnd();
+ 
+    /**
+       Optional function: called at the beggining of each iterative loop.
+       This provides all clusters' information in case the algorithm need them. Note this
+       is called per iteration which may be more than once per event.
+     */
+    //virtual void IterationBegin(const std::vector<cluster::ClusterParamsAlgNew> &clusters);
+
+    /**
+       Optional function: called at the end of each iterative loop.
+     */
+    //virtual void IterationEnd();
+
+
+   private:
+	
+	float TProfCompare(std::vector<larutil::PxHit> hita ,std::vector<larutil::PxHit> hitb);	
+
+	
+
+  };
+}
+#endif
+/** @} */ // end of doxygen group 
+
