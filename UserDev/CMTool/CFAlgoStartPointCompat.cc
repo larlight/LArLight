2da91d6d38d2c805e95456e4274c3d2544a73779
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-09-05 09:24:49 -0500

    Update protection against 2-plane combination --kazu

--- UserDev/CMTool/CFAlgoStartPointCompat.cc
+++ UserDev/CMTool/CFAlgoStartPointCompat.cc
@@ -28,10 +28,9 @@ namespace cmtool {
   {
 
     // Code-block by Kazu starts
-    // This ensures all entries in "clusters" pointer vector are valid pointers.
+    // This ensures the algorithm works only if # clusters is > 2 (and not =2)
     // You may take out this block if you want to allow matching using clusters from only 2 planes.
-    // But make sure you handle the case of null pointer
-    for(auto const& ptr : clusters) if(!ptr) return -1;
+    if(clusters.size()==2) return -1;
     // Code-block by Kazu ends
 
     //This algorithm now works for 3 planes: find 3Dstart point from first 2 planes and find
