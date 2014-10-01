0a24c33978f7fd094864f4f6b8547fa4fe1b50e9
David <David Caratelli>
2014-10-01 10:18:52 -0500

    Added HitBacktracker class to CMToolAna: Finds simch IDEs associated with a hit. Basically just one of the steps done in McShowerlookback --David Caratelli

--- UserDev/CMToolAna/LinkDef.h
+++ UserDev/CMToolAna/LinkDef.h
@@ -18,6 +18,7 @@
 #pragma link C++ class ::larlight::ComputePi0Mass_RG+;
 #pragma link C++ class ::larlight::MCShowerMatchAlg+;
 #pragma link C++ class ::larlight::CMToolAnaException+;
+#pragma link C++ class ::larlight::HitBackTracker+;
 #pragma link C++ class TwoShower3DIntxn+;
 
 //ADD_NEW_CLASS ... do not change this line
