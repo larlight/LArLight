d5a323ebec14c7babfee93a20bf8d9fef8bab541
David <David Caratelli>
2014-10-30 23:16:38 -0500

    edits to SNCompression --David Caratelli

--- UserDev/SNCompression/CompressionAlgoBase.cc
+++ UserDev/SNCompression/CompressionAlgoBase.cc
@@ -9,8 +9,14 @@ namespace compress {
 
     /// This algorithm simply returns the original waveform as the compressed one! Not very useful!
     
+    //Set the Output waveform to be identical to the input waveform
     _OutWF.push_back(waveform);
+    // Set the start tick for the output waveform to be 0
+    // if this were something actually serious/useful
+    // we should set the start-tick to be the time-tick
+    // in the old waveform at which the new waveform starts
     _OutWFStartTick.push_back(0);
+
     return;
   }
   
