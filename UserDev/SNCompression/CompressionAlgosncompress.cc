63ff504f53f61edff42d23ec1016931dcfbf03ac
Connor Callahan <csc2168@houston.nevis.columbia.edu>
2014-11-21 16:28:59 -0500

    Corrections

--- UserDev/SNCompression/CompressionAlgosncompress.cc
+++ UserDev/SNCompression/CompressionAlgosncompress.cc
@@ -7,20 +7,20 @@ namespace compress {
   
   void CompressionAlgosncompress::ApplyCompression(const std::vector<unsigned short> &waveform, int mode){
 
-    int baseline = 0;
+    int base = 0;
     int buffer = 0;
 
     //0 -> Collection
     if(mode == 0){
-      baseline = 400;
+      base = 400;
       buffer = 3;
     }
     else if(mode == 1){
-      baseline = 2048;
+      base = 2048;
       buffer = 5;
     }
     else{
-      baseline = 2048;
+      base = 2048;
       buffer = 10;
     }
 
@@ -38,58 +38,64 @@ namespace compress {
     
     for(int pos = 0; pos< 9600; pos+= 64){
       save = 0;
+      for(int j = 0; j<3; j++){
+	variance[j] = 0;
+	baseline[j] = 0;
+      }
       //find baseline
       for(i = pos;i < pos + 64; i++){
 	baseline[0] += waveform[i];
       }
-      for( i; i< pos +128 ; i++){
+      for(; i< pos +128 ; i++){
 	baseline[1] += waveform[i];
       }
-      for(i ; i< pos +192 ; i++){
+      for( ; i< pos +192 ; i++){
 	baseline[2] += waveform[i];
       }
       
       //find variance
-      for(i; i< pos + 64; i++){
+      for(; i< pos + 64; i++){
 	diff = abs(waveform[i] - baseline[0]);
-	if(diff < 63) {variance[i] += diff*diff}; 
-	else{ variance[i] += 4095};
+	if(diff < 63) {variance[1] += diff*diff;} 
+	else{ variance[1] += 4095;}
       }
-      for(i; i< pos + 128; i++){
+      for(; i< pos + 128; i++){
 	diff = abs(waveform[i] - baseline[0]);
-	if(diff < 63) {variance[i] += diff*diff}; 
-	else{ variance[i] += 4095};
+	if(diff < 63) {variance[2] += diff*diff;} 
+	else{ variance[2] += 4095;}
       } 
-      for( i; i< pos +192; i++){
+      for( ; i< pos +192; i++){
 	diff = abs(waveform[i] - baseline[0]);
-	if(diff < 63) {variance[i] += diff*diff}; 
-	else{ variance[i] += 4095};
+	if(diff < 63) {variance[3] += diff*diff;} 
+	else{ variance[3] += 4095;}
       }
       
       for(int j = 0; j<3; j++){
-	baseline[i] = baseline[i]/64;
-	variance[i] = variance[i]/64;
+	baseline[j] = baseline[j]/64;
+	variance[j] = variance[j]/64;
       }
       
-      for(i; i< pos + 256; i++){
-	if(waveform[i] > baseline + c){
-	  save = i;
-	}
-	else if(waveform[i] < baseline - c){
-	  save = i;
+      for(int j = 0; j<3 ; j++){
+	for(; i< pos + 256; i++){
+	  if(waveform[j] > base + c){
+	    save = i;
+	  }
+	  else if(waveform[j] < base - c){
+	    save = i;
+	  }
 	}
       }
 
       //for now calling the whole range of pre and post N to be an 8-tick block
       if(save > 0){
-	for(save - buffer; save + buffer; save++){
-	  outputwf.push_back(waveform[save]);
+	for(int j =save - buffer; j<save + buffer; j++){
+	  outputwf.push_back(waveform[j]);
 	}
       }
       
       if(baseline[2] - baseline[1] < deltaB && baseline[2] - baseline[0] < deltaB && baseline[1] - baseline[0] < deltaB){
 	if(variance[2] - variance[1] < deltaV && variance[2] - variance[0] < deltaV && variance[1] - variance[0] < deltaV){
-	  baseline = baseline[1];
+	  base = baseline[1];
 	}
       }
     }
